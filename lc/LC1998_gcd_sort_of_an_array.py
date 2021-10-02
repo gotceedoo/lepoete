3f79b6fc3d5b87acaeb31d0658ff0dbf4571d0fb
U-f5\yb <fans.groups@outlook.com>
2021-10-02 21:39:18 +0800

    revert LC0004

--- lc/LC1998_gcd_sort_of_an_array.py
+++ lc/LC1998_gcd_sort_of_an_array.py
@@ -1,22 +1,13 @@
 def gcdSort(self, nums):
     def find(n):
-        if uf[n] != n:
-            uf[n] = find(uf[n])  # path compression
+        if uf[n] != n: uf[n] = find(uf[n])                             # path compression
         return uf[n]
 
     uf, vis, seen = list(range(100001)), [False] * 100001, set(nums)
-    for i in range(
-        2, 100001
-    ):  # iterate over successive natual numbers beginning with 2
-        if vis[i]:
-            continue  # skip visited including any composite number
-        for j in range(i, 100001, i):  # iterate over the prime and its multiples
-            vis[j] = True  # mark them visited
-            if j in seen:
-                uf[find(j)] = find(
-                    i
-                )  # union connected numbers containing same prime factor
+    for i in range(2, 100001):                                         # iterate over successive natual numbers beginning with 2
+        if vis[i]: continue                                            # skip visited including any composite number
+        for j in range(i, 100001, i):                                  # iterate over the prime and its multiples
+            vis[j] = True                                              # mark them visited
+            if j in seen: uf[find(j)] = find(i)                        # union connected numbers containing same prime factor
 
-    return all(
-        find(x) == find(y) for x, y in zip(nums, sorted(nums))
-    )  # swappable if each of pair numbers connected
+    return all(find(x) == find(y) for x, y in zip(nums, sorted(nums))) # swappable if each of pair numbers connected
