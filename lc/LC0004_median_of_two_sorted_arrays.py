ce84869d550f77f54765218bfe717d787c649675
sprint4us <sprint21er@googlemail.com>
2021-10-03 04:07:35 +0200

    LC0004: needn't choose the shorter array

--- lc/LC0004_median_of_two_sorted_arrays.py
+++ lc/LC0004_median_of_two_sorted_arrays.py
@@ -1,13 +1,11 @@
 def findMedianSortedArrays(_, nums1, nums2):
-    n1, n2 = len(nums1), len(nums2)
-    if n1 > n2:
-        nums1, nums2 = nums2, nums1
+    l1, r1, n2 = 0, len(nums1), len(nums2)
+    m, k = divmod(r1 + n2 - 1, 2)
 
-    (m, k), l1, r1 = divmod(n1 + n2 - 1, 2), 0, min(n1, n2)
     while l1 < r1:
         m1 = (l1 + r1) // 2
         m2 = m - 1 - m1
-        if m2 < 0 or nums1[m1] > nums2[m2]:
+        if m2 < 0 or m2 < n2 and nums1[m1] > nums2[m2]:
             r1 = m1
         else:
             l1 = m1 + 1
