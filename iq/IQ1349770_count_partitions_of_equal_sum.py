a628adac25e1ce8758a00c27caa8bd7bf16f9600
gotceedoo <fans.groups@outlook.com>
2021-10-09 11:33:36 +0800

    IQ1349770: rename and unindent

--- iq/IQ1349770_count_partitions_of_equal_sum.py
+++ iq/IQ1349770_count_partitions_of_equal_sum.py
@@ -3,10 +3,10 @@ def count_partitions_of_equal_sum(nums):
     prefix = collections.Counter(list(itertools.accumulate(nums))[:-1])
     suffix = collections.Counter(list(itertools.accumulate(nums[::-1]))[:-1])
     for num in nums:
-        changed, odd = divmod(total - num, 2)
+        changed, skip = divmod(total - num, 2)
+        if skip: continue
         unchanged = changed + num
-        if not odd:
-            this = prefix[changed] and suffix[unchanged]
-            that = num and suffix[changed] and prefix[unchanged]
-            count = max(count, this + that)
+        this = prefix[changed] and suffix[unchanged]
+        that = num and suffix[changed] and prefix[unchanged]
+        count = max(count, this + that)
     return count
