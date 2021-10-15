1f35e00cf5f04843b03c1886db4ef54a503cbdbb
gotceedoo <fans.groups@outlook.com>
2021-10-16 00:46:51 +0800

    LC0315: calc logN

--- lc/LC0315_count_of_smaller_numbers_after_self.py
+++ lc/LC0315_count_of_smaller_numbers_after_self.py
@@ -1,14 +1,13 @@
 def countSmaller(self, nums):
-    n, size = 1, len(nums)
+    size = len(nums)
     A, dp = list(range(size)), [0] * size
-    while n < size:
-        for i in range(0, size, n * 2):
-            L, R = A[i : i + n], A[i + n : i + n * 2]
+    for n in range(math.ceil(math.log(size, 2))):
+        for i in range(0, size, 2 ** (n + 1)):
+            L, R = A[i : i + 2 ** n], A[i + 2 ** n : i + 2 ** (n + 1)]
             for j in range(i, i + len(L) + len(R))[::-1]:
                 if not R or L and nums[L[-1]] > nums[R[-1]]:
                     dp[L[-1]] += len(R)
                     A[j] = L.pop()
                 else:
                     A[j] = R.pop()
-        n *= 2
     return dp
