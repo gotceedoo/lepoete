52a9cadf40c4939df0d8e679fd259884b93e4497
gotceedoo <fans.groups@outlook.com>
2021-10-25 21:20:06 +0800

    LC0327: set size of half partition

--- lc/LC0327_count_of_range_sum.py
+++ lc/LC0327_count_of_range_sum.py
@@ -4,18 +4,19 @@ def countRangeSum(self, nums, lower, upper):
 
     for n in range(math.ceil(math.log(size, 2))):               # iterate to mergesort logN times
 
-        for l in range(0, size, 2 ** (n + 1)):                  # iterate over partitions
+        n = 2 ** n                                              # set size of half partition
+        for l in range(0, size, 2 * n):                         # iterate over partitions
 
-            i = j = l + 2 ** n                                  # locate the start of two pointers, i.e. the middle per partition
-            r = min(l + 2 ** (n + 1), size)                     # locate the end of two pointers, i.e. the right per partition
-            L, R = A[l:i], A[i:r]                               # prepare left and right parts per partition
+            i = j = m = l + n                                   # set beginning of two pointers, i.e. middle of partition
+            r = min(m + n, size)                                # set end of two pointers, i.e. right of partition
+            for k in range(l, m):                               # iterate over left half
+                while i < r and A[i] - A[k] <  lower: i += 1    # step forward till lower boundary satisfied over right half
+                while j < r and A[j] - A[k] <= upper: j += 1    # step forward till upper boundary unsatified over right half
+                res += j - i                                    # add up count of range sum satisfied beginning w/ kth element over left half
 
-            for x in L:                                         # iterate over left part per partition
-                while i < r and A[i] - x <  lower: i += 1       # step forward till lower boundary satisfied over right part per partition
-                while j < r and A[j] - x <= upper: j += 1       # step forward till upper boundary unsatified over right part per partition
-                res += j - i                                    # add up count of range sum satisfied per element
-
+            L, R = A[l:m], A[m:r]                               # prepare left and right halves
             for i in range(l, r)[::-1]:                         # execute mergesort per partition
                 if not R or L and L[-1] > R[-1]: A[i] = L.pop()
                 else:                            A[i] = R.pop()
+
     return res
