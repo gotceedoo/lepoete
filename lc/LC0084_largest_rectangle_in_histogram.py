3798e7a6bf3e251d2ea28e8e1d20d042b4890544
gotceedoo <fans.groups@outlook.com>
2021-11-09 00:20:02 +0800

    LC0084: A.pop() -> H[A.pop()]

--- lc/LC0084_largest_rectangle_in_histogram.py
+++ lc/LC0084_largest_rectangle_in_histogram.py
@@ -2,6 +2,6 @@ def largestRectangleArea(self, heights):
     res, A, H = 0, [], [0] + heights + [0]
     for i, h in enumerate(H):
         while A and H[A[-1]] > r:
-            res = max(res, A.pop() * (i - 1 - A[-1]))
+            res = max(res, H[A.pop()] * (i - 1 - A[-1]))
         A.append(i)
     return res
