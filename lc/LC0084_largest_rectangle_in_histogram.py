0c3694c1caba3d6a42134c3878ca126c853cac10
gotceedoo <fans.groups@outlook.com>
2021-11-10 11:58:57 +0800

    LC0084: optimize and comment]

--- lc/LC0084_largest_rectangle_in_histogram.py
+++ lc/LC0084_largest_rectangle_in_histogram.py
@@ -1,7 +1,26 @@
-def largestRectangleArea(self, heights):
-    res, A, H = 0, [], [0] + heights + [0]
-    for i, h in enumerate(H):
-        while A and H[A[-1]] > r:
-            res = max(res, H[A.pop()] * (i - 1 - A[-1]))
-        A.append(i)
+def largestRectangleArea(self, heights): # sliding window w/ increasing mono-stack O(N)
+    res = 0
+    heights.append(0)                    # end at height 0 to ensure all heights processed later;
+    asc = [-1]                           # stack will hold indices of increasing heights,
+                                         # and begins w/ index of last height 0 to ensure itself non-empty,
+                                         # thus avoid nullness check later.
+
+    for j, r in enumerate(heights):      # r:   current right height;
+                                         # j:   index of current right height and wannabe current right of rectangle width;
+                                         # all indices will be pushed into stack and popped off exactly once, thus O(N).
+
+        while heights[asc[-1]] > r:      # expand current window backward;
+                                         # we will pop off indices of left heights greater than current right height,
+                                         # calculate and maximize rectangle areas accordingly;
+
+            l = heights[asc.pop()]       # l:   current rectangle height popped off, which is decreasing;
+            i = asc[-1] + 1              # i:   current left of rectangle width, which is decreasing;
+            res = max(res, l * (j - i))  # res: running local maximum rectangle area,
+                                         #      which is w/ decreasing height but increasing width in current window.
+
+        asc.append(j)                    # move next window forward;
+                                         # append index of current right height,
+                                         # which is always greater than or equal to all remaining left heights.
+
     return res
+
